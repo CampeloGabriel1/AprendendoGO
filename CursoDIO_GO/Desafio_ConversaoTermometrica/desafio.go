ad6b49f68b9ecea8c72134cde53e071e22584c03
CampeloGabriel1 <campelo1gabriel@gmail.com>
2025-12-26 20:16:10 -0300

    feat: add temperature conversion from Kelvin to Celsius in desafio.go

---
+++ CursoDIO_GO/Desafio_ConversaoTermometrica/desafio.go
@@ -0,0 +1,14 @@
+package main
+
+import "fmt"
+
+const ebulicaoK int = 373
+
+func main() {
+
+
+	k := ebulicaoK
+	c := k - 273
+	fmt.Printf("O valor em Kelvin é %d e o valor em Celsius é %d", k, c)
+
+}
