b203ce032e59a4503bfe2e9a16fd62f2d4181f93
CampeloGabriel1 <campelo1gabriel@gmail.com>
2026-01-01 19:04:40 -0300

    Criando meu primeiro servidor estático em Go

---
+++ CursoDIO_GO/servidorestatico/servidor.go
@@ -0,0 +1,20 @@
+package main
+
+import (
+	"log"
+	"net/http"
+)
+
+func main() {
+	fs := http.FileServer(http.Dir("./estatico"))
+	http.Handle("/", fs)
+
+	log.Print("Listening on :8080...",)
+
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+
+}
